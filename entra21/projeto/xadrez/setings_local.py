a01c1c516cb91c57792baed9b3ca7c095a967559
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-08-27 15:34:56 -0300

    add SECRET_KEY
    adicionei a SECRET_KEY para resolver os erros causados pela falta dela

--- entra21/projeto/xadrez/setings_local.py
+++ entra21/projeto/xadrez/setings_local.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # SECURITY WARNING: keep the secret key used in production secret!
+=======
+# SECURITY WARNING: keep the secret key used in production secret!
+>>>>>>> 791b7069117ed34df05fc38fbb96c86c8228d550
 SECRET_KEY = 'django-insecure-ryd%zs#4lshl))xhosjv4lg+u8k!yo=lla3--+_x-j4^@-va02'
