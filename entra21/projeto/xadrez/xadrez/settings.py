afba796d1e39b02faeb971f874daa5aba06297f9
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-08-28 16:21:16 -0300

    Update front

--- entra21/projeto/xadrez/xadrez/settings.py
+++ entra21/projeto/xadrez/xadrez/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'xadrez.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR  / "main/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
