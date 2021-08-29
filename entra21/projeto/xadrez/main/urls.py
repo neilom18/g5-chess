4649c7067c0bf95c1b8c26d0422b91fa274b8102
unknown <leandropamplona2@gmail.com>
2021-08-29 20:37:46 -0300

    Paginas sala e historico
    Criei as paginas historico e sala, e dei um include das urls da pasta main para as urls da pasta xadrez

--- entra21/projeto/xadrez/main/urls.py
+++ entra21/projeto/xadrez/main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls.conf import include
 from .import views
 from .views import *
 
@@ -6,4 +7,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('sala', views.sala, name = 'sala'),
+    path('historico', views.historico, name = 'historico')
 ]
--- entra21/projeto/xadrez/main/views.py
+++ entra21/projeto/xadrez/main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def sala(request):
+    return render(request, 'sala.html')
+
+def historico(request):
+    return render(request, 'historico.html')
