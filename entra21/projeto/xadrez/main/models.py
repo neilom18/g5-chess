22c07fd0a9bbc4b351a36b8f2cbb1042ad594966
unknown <leandropamplona2@gmail.com>
2021-09-08 15:11:02 -0300

    Retirei o Base.html

--- entra21/projeto/xadrez/main/models.py
+++ entra21/projeto/xadrez/main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
