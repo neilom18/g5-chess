205bbe752716f7c3ee672cd1f29b1c6e6d618b5d
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-09-08 14:30:28 -0300

    add model: Sala
    Add modelo Sala, recebe code e user

--- entra21/projeto/xadrez/main/models.py
+++ entra21/projeto/xadrez/main/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
+
+class Sala(models.Model):
+    code = models.CharField(max_length=9,default="",unique=True)
+    user = models.ForeignKey(User, related_name = 'user', null = False, on_delete = models.CASCADE)
+
+
