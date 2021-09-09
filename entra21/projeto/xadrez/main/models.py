084cbc75ea2117d3e45893b85ed4f77584e63290
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-09-09 15:33:56 -0300

    lista_salas update

--- entra21/projeto/xadrez/main/models.py
+++ entra21/projeto/xadrez/main/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
 
 class Sala(models.Model):
-    code = views.random_code()
+    code = models.CharField(max_length=9,default="",unique=True)
     user = models.ForeignKey(User, related_name = 'user', null = False, on_delete = models.CASCADE)
 
     def __str__(self):
