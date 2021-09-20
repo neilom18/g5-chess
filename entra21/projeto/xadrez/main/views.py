f9f956c51ba8773a67033e72720a1ccf9921e1ff
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-09-20 13:05:23 -0300

    Modifiquei o user_id
    Agora no momento do cadastro é gerado um id aleatório de 16 digitos, contendo letras e números.

--- entra21/projeto/xadrez/main/views.py
+++ entra21/projeto/xadrez/main/views.py
@@ -15,3 +15,18 @@ def historico(request):
 
 def perfil(request):
     return render(request, 'perfil.html')
+
+def ramRoom():
+    alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    r = []
+    for i in range(16):
+        i = random.randint(1,2)
+        if i == 1:
+            n = random.randint(1,9)
+            r.append(n)
+        else:
+            a = random.choice(alph)
+            r.append(a)
+    r = ''.join(map(str, r))
+    return r
+
