195192f6397d0fb31d0fd2a7d63b2e4c0dfa82d5
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-09-20 14:08:01 -0300

    correção de erro

--- entra21/projeto/xadrez/main/models.py
+++ entra21/projeto/xadrez/main/models.py
@@ -1,4 +1,4 @@
-from main.views import ramRoom
+from main.views import ramCode
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import datetime
@@ -16,8 +16,8 @@ class Sala(models.Model):
 
 class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
-    id = models.CharField(max_length=16,primary_key=True, default=ramRoom())
+    id = models.CharField(max_length=16,primary_key=True, default=ramCode())
     sala = models.ForeignKey(Sala, related_name = 'sala', null=True, on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.sala
+        return self.id
--- entra21/projeto/xadrez/main/views.py
+++ entra21/projeto/xadrez/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import random
-
+#Template request
 def home(request):
     return render(request, 'home.html')
 
@@ -15,8 +15,10 @@ def historico(request):
 
 def perfil(request):
     return render(request, 'perfil.html')
+#Template request
 
-def ramRoom():
+
+def ramCode():
     alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     r = []
     for i in range(16):
