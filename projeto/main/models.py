8df85581b05ab4e73fff20bc040876345dcbb548
Lirio145 <89553307+nomeBom@users.noreply.github.com>
2021-09-21 12:46:23 -0300

    correção de erros

--- projeto/main/models.py
+++ projeto/main/models.py
@@ -5,18 +5,19 @@ import datetime
 
 
 class Sala(models.Model):
-    code = models.CharField(max_length=9, default="0",unique=True)
+    nome = models.CharField(max_length=100, default='sala')
+    id = models.CharField(max_length=16,primary_key=True, default=ramCode,unique=True)
     d_criacao = models.DateTimeField(auto_now_add=True)
 
     def sala_code(self):
-        return self.code
+        return self.nome
 
     def __str__(self):
         return self.code
 
 class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
-    id = models.CharField(max_length=16,primary_key=True, default=ramCode())
+    id = models.CharField(max_length=16,primary_key=True, default=ramCode,unique=True)
     sala = models.ForeignKey(Sala, related_name = 'sala', null=True, on_delete = models.CASCADE)
 
     def __str__(self):
