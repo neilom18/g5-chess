d5961b850124869bf820702f00c4380f87ef928f
Lirio145 <89553307+NarueGS@users.noreply.github.com>
2021-09-22 09:17:12 -0300

    Adicionei o campo userCode e a função ramCode
    Adicionei o campo userCode, campo não nulo de 16 digitos numéricos e não numéricos aleatórios gerados pela função ramCode,  e a função ramCode, sorteia aleatóriamente 16 digitos numéricos e não numéricos e os returna como uma só string.

--- projeto/main/models.py
+++ projeto/main/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import random
+
+def ramCode():
+    alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    r = []
+    for i in range(16):
+        i = random.randint(1,2)
+        if i == 1:
+            n = random.randint(1,9)
+            r.append(n)
+        else:
+            a = random.choice(alph)
+            r.append(a)
+    r = ''.join(map(str, r))
+    return r
 
 start = ' rw00 cw01 bw02 qw03 kw04 bw05 cw06 rw07 pw10 pw11 pw12 pw13 pw14 pw15 pw16 pw17 pb60 pb61 pb62 pb63 pb64 pb65 pb66 pb67 rb70 cb71 bb72 qb73 kb74 bb75 cb76 rb77'
 
@@ -11,9 +26,9 @@ class Room(models.Model):
 #user
 class User(AbstractUser):
     pais = models.CharField(max_length=100, blank=True,default='')
+    userCode = models.CharField(max_length=10,default=ramCode,)
     room = models.ForeignKey(Room, related_name = 'room', null=True, on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.id
-
+        return self.userCode
 
