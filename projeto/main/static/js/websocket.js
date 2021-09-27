1a709ae2a5dd276c0c43cb31426398d4a3701a78
thelikeaninja <77681263+thelikeaninja@users.noreply.github.com>
2021-09-27 19:38:26 -0300

    aaa

--- projeto/main/static/js/websocket.js
+++ projeto/main/static/js/websocket.js
@@ -1,5 +1,6 @@
-
+let squares = document.querySelectorAll('.quadrado')
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
+const startoooloo = 'rw00 cw01 bw02 qw03 kw04 bw05 cw06 rw07 pw10 pw11 pw12 pw13 pw14 pw15 pw16 pw17 pb60 pb61 pb62 pb63 pb64 pb65 pb66 pb67 rb70 cb71 bb72 qb73 kb74 bb75 cb76 rb77'
 // cria o webscoket
 const chatSocket = new WebSocket(
     'ws://'
@@ -11,13 +12,12 @@ const chatSocket = new WebSocket(
 // recebe a mensagem do websocket
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-   console.log(data)
     if(data.message){
         document.querySelector('#chat-log').value += (data.message + '\n');
 
     }
     if(data.piece){
-        console.log(data.piece)
+        drawPieces(startoooloo)
     }
     else{
         console.log(data)
@@ -48,3 +48,19 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
     }));
     messageInputDom.value = '';
 };
+
+function drawPieces(allPieces){
+    allPiecesArray = allPieces.split(' ')
+    for (let i = 0; i<allPiecesArray.length;i++){
+        const piece = allPiecesArray[i]
+        pieceCoord = piece[2]+piece[3]
+        squares = Array.from(squares)
+        squares.map((square) => {
+            if(square.dataset.id == pieceCoord){
+                square.innerHTML = `<img class='square' src='./pecas/${piece[0]}${piece[1]}'>`
+                return;
+            }
+            return;
+        })
+    }
+}
